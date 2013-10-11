37d5708ddd047222df7be240adbf6cc821fc11cc
zhixing <zhixing135@hotmail.com>
2013-10-12 03:23:04 +0800

    Replaced old project with new project files

---
+++ BigSpoonDiner/BigSpoonDiner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BigSpoonDiner
+//
+//  Created by Zhixing Yang on 12/10/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BigSpoonDiner/BigSpoonDiner/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  TabbedViewController
+//
+//  Created by Zhixing Yang on 6/10/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
