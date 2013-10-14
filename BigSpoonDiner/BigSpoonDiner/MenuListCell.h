77177bf14438a276a6592b96cbfecf2bcc3084e9
zhixing <zhixing135@hotmail.com>
2013-10-15 04:42:42 +0800

    Added prototypal cells for menu list and menu photo display

---
+++ BigSpoonDiner/BigSpoonDiner/MenuListCell.h
@@ -0,0 +1,18 @@
+//
+//  MenuListCell.h
+//  BigSpoonDiner
+//
+//  Created by Zhixing Yang on 15/10/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuListCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *priceLabel;
+@property (strong, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (strong, nonatomic) IBOutlet UIButton *addButton;
+- (IBAction)addButtonPressed:(id)sender;
+
+@end
