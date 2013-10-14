b419ed4db4ce670101119069007034249974b048
zhixing <zhixing135@hotmail.com>
2013-10-15 02:23:28 +0800

    Added sign-up page and added fields in the log-in page

---
+++ BigSpoonDiner/BigSpoonDiner/AuthViewController.h
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.h
+//  BigSpoonDiner
+//
+//  Created by Zhixing Yang on 15/10/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuthViewController : UIViewController
+
+@end
---
+++ BigSpoonDiner/BigSpoonDiner/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  BigSpoonDiner
+//
+//  Created by Zhixing Yang on 15/10/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
