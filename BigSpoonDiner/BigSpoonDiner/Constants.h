5e373670cb026e557816f6b4d9d1683f004a0ca6
zhixing <zhixing135@hotmail.com>
2013-10-31 20:58:02 +0800

    Integrated with Order API. Able to place order now

--- BigSpoonDiner/BigSpoonDiner/Constants.h
+++ BigSpoonDiner/BigSpoonDiner/Constants.h
@@ -16,6 +16,7 @@ extern NSString* const  USER_SIGNUP;
 extern NSString* const  USER_LOGIN;
 extern NSString* const  LIST_OUTLETS;
 extern NSString* const REQUEST_URL;
+extern NSString* const ORDER_URL;
 
 // Others:
 extern int const ROW_HEIGHT_LIST_MENU;
