cc81073bf9dc1313931930bcd388b056d86ce163
zhixing <zhixing135@hotmail.com>
2013-11-05 10:00:10 +0800

    Changed the style of add and minus buttons.

--- BigSpoonDiner/BigSpoonDiner/MenuTableViewController.m
+++ BigSpoonDiner/BigSpoonDiner/MenuTableViewController.m
@@ -107,7 +107,7 @@
         // When the button is clicked, we know which one. :)
         cell.addButton.tag = dish.ID;
     
-        cell.priceLabel.text = [NSString stringWithFormat:@"$%d", dish.price];
+        cell.priceLabel.text = [NSString stringWithFormat:@"%d", dish.price];
     
         cell.descriptionLabel.text = dish.description;
         
@@ -139,7 +139,7 @@
         
         cell.nameLabel.text = dish.name;
         
-        cell.priceLabel.text = [NSString stringWithFormat:@"$%d", dish.price];
+        cell.priceLabel.text = [NSString stringWithFormat:@"%d", dish.price];
         
         cell.descriptionLabel.text = dish.description;
         
