e44113545dcba9be5ee86421e002cdde27098b2f
zhixing <zhixing135@hotmail.com>
2013-11-19 12:02:09 +0800

    Stable version for demo

--- BigSpoonDiner/BigSpoonDiner/UIViewController+KeyboardEvents.h
+++ BigSpoonDiner/BigSpoonDiner/UIViewController+KeyboardEvents.h
@@ -14,10 +14,12 @@
 // Usage:
 
 // Define these metnods in the ViewController:
+//
 // Bind the textField action to them:
-// ==>
-// ==>
-// ==>
+//
+// Edit Did Begin ==> textFieldDidBeginEditing
+// Editing Did End ==> textFinishEditing
+// Did End on Exit (this is when user press "return" button) ==> textFinishEditing
 
  /*
 - (IBAction)textFieldDidBeginEditing:(id)sender {
