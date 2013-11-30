6e237f6cef27dcb5bb2e50c92a91f218e1396420
Qiao Liang <qiaoliang89@gmail.com>
2013-11-30 14:43:14 +0800

    added email feedback btn in setting

--- BigSpoonDiner/BigSpoonDiner/OrderHistoryViewController.h
+++ BigSpoonDiner/BigSpoonDiner/OrderHistoryViewController.h
@@ -11,12 +11,14 @@
 #import "Constants.h"
 #import "User.h"
 #import "PastOrderView.h"
+#import <MessageUI/MessageUI.h> 
 
 @class OrderHistoryViewController;
 
-@interface OrderHistoryViewController : UIViewController <NSURLConnectionDelegate>
+@interface OrderHistoryViewController : UIViewController <NSURLConnectionDelegate, MFMailComposeViewControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIScrollView *orderHistoryScrollView;
 @property (weak, nonatomic) HomeAndSettingsButtonView *topRightButtonsView;
+@property (weak, nonatomic) IBOutlet UILabel *suggestionLabel;
 
 @end
